fdbbc4bb72595a41dea296bf8f2a1740bd9c7d70
HToy91 <jfvargasfigueroa@email.neit.edu>
2026-04-27 12:11:07 -0400

    Add W5/midterm React + Vite project
    Scaffold a new W5/midterm React + TypeScript project using Vite. Adds source files (App, main, styles, assets), project configs (tsconfig, vite.config.ts, eslint.config.js), index.html, README, sample db.json for development, .gitignore files, and IDE (.idea) configuration files.

---
+++ W5/midterm/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className={"nav-bar"}>
+          <div>Home</div>
+          <div>Products</div>
+      </div>
+    </>
+  )
+}
+
+export default App
